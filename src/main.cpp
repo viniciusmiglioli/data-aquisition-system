0bfd7197ec3355684bc37d0dce2371241558c52b
viniciusmiglioli <86131188+viniciusmiglioli@users.noreply.github.com>
2023-05-16 23:02:13 +0000

    modified:   src/main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,12 @@
+#include <cstdlib>
 #include <iostream>
+#include <memory>
+#include <utility>
+#include <fstream>
 #include <boost/asio.hpp>
+
+using boost::asio::ip::tcp;
+
 
 int main(int argc, char* argv[]) {
     return 0;
